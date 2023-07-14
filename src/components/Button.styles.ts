3de38375773ac77ec6fa5658ee3720ade85e40e2
Wellington dos Santos Silva <106429237+wffwds@users.noreply.github.com>
2023-07-14 19:56:34 -0400

    Estrutura da aplicação - Styled Components

---
+++ src/components/Button.styles.ts
@@ -0,0 +1,24 @@
+import styled, { css } from 'styled-components'
+
+export type ButtonVariant = 'primary'  | 'secondary'  | 'danger'  |  'success'
+
+interface ButtonContainerProps{
+  variant: ButtonVariant
+}
+
+const buttonVariant = {
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'green',
+  success: 'green'
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+  ${props => {
+    return css`
+      background-color: ${buttonVariant[props.variant]}
+    `
+  }}
+`
