d48c7bd1a36552e9472f3987724ab32502221882
Wellington dos Santos Silva <106429237+wffwds@users.noreply.github.com>
2023-07-14 21:54:37 -0400

    Estrutura da aplicação - Configurando Temas ESLint

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,8 +1,8 @@
 import styled, { css } from 'styled-components'
 
-export type ButtonVariant = 'primary'  | 'secondary'  | 'danger'  |  'success'
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
-interface ButtonContainerProps{
+interface ButtonContainerProps {
   variant: ButtonVariant
 }
 
@@ -10,15 +10,20 @@ const buttonVariant = {
   primary: 'purple',
   secondary: 'orange',
   danger: 'green',
-  success: 'green'
+  success: 'green',
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-  ${props => {
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
+  border-radius: 0.5rem;
+  margin: 0.5rem;
+  border: 0;
+  /*${(props) => {
     return css`
-      background-color: ${buttonVariant[props.variant]}
+      background-color: ${buttonVariant[props.variant]};
     `
-  }}
+  }}*/
 `
